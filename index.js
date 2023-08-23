285435d29b98f55f4f4f1ecd00ccfdbc15afed14
brunotop <142436190+brunotop@users.noreply.github.com>
2023-08-23 17:26:57 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,104 @@
+/*
+let a = 3;
+let b = 5;
+let c = 6;
+let d = 7;
+let e = 4;
+console.log(a % 2);
+console.log(b % 2);
+console.log(c % 2);
+console.log(d % 2);
+console.log(e % 2);
+*/
+/*
+let minhaIdade = 26;
+console.log("Antes", minhaIdade);
+minhaIdade = minhaIdade + 1;
+console.log("Depois", minhaIdade);
+
+let n1 = 3;
+let n2 = 4;
+let tot = n1 + n2;
+console.log("O resultado da soma é", tot);
+
+let m1 = 3;
+let m2 = 5;
+let resul = (m1 * m2) /2;
+console.log("O valor total é", resul);
+
+let valor1 = 5;
+let valor2 = 4;
+let to = (valor1 - valor2) *-1;
+console.log("O total é", to);
+let d1 = 234;
+let d2 = 5;
+let total = d1 % d2;
+console.log("O resto de", d1, "divididdo por", d2, "é", total);
+//
+let a = 3;
+let b = 4;
+console.log(a + b);
+let c = 3;
+let d = 5;
+let e = 2;
+let f = -1;
+console.log(c * d /2);
+console.log((d - b) * f);
+let g = 234;
+let h = 5;
+console.log(g % h );
+*/
+//
+/*
+let n1 = 2;
+let n2 = 3;
+console.log("igual", n1 === n2);
+console.log("diferente",n1 !== n2);
+console.log("maior",n1 > n2);
+console.log("menor",n1 < n2);
+//comparadores
+let a = 5;
+let b = 3;
+let c = 0;
+console.log(a===b || a !== c);
+console.log(a===b && a !== c);
+console.log("---- separador ----");
+|| !
+console.log(true || false);
+console.log(true && false);
+console.log(true && true);
+*/
+/*
+let v1 = true;
+let v2 = false;
+let v3 = true;
+console.log(v1 && v2);
+console.log(v2 && v3);
+console.log(v1 && v3);
+console.log(v1 && v2 && v3);
+
+let v1 = true;
+let v2 = false;
+let v3 = true;
+console.log(v1 || v2);
+console.log(v2 || v3);
+console.log(v1 || v3);
+console.log(v1 || v2 || v3);
+
+//exercicio
+ let a = true;
+let b = false;
+console.log(a);
+console.log(!a);
+console.log(b);
+console.log(!b);
+*/
+//
+
+let nome = prompt("Qual seu nome?");
+console.log(nome);
+let idade = Number(prompt("Qual sua idade?"));
+console.log(idade);
+console.log(idade >= 18);
+console.log(idade + 27);
+
